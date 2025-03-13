74b5672d277f1c565faa565d574c8caffb63df9e
suting777 <suting.w777@gmail.com>
2025-03-13 21:00:16 +0000

    login on CHrome and Firefox

--- login_cross_brosers_selenium.py
+++ login_cross_brosers_selenium.py
@@ -4,21 +4,21 @@ from selenium.webdriver.common.by import By
 browers = ["Chrome","Firefox"]
 
 for brower in browers:
-    if brower == "Chorme":
+    if brower == "Chrome":
       driver = webdriver.Chrome()
-    else browser == "Firefox":
+    elif browser == "Firefox":
       driver = webdriver.Firefox()
 
-driver.get("https://www.saucedemo.com/")
-print(running test on {browser}
+    driver.get("https://www.saucedemo.com/")
+    print(f"running test on {browser}")
 
-username = driver.find_element(By.ID, "user-name")
-password = driver.find_element(By.ID, "password")
-username.send_keys("standard_user")
-password.send_keys("secret_sauce")
+    username = driver.find_element(By.ID, "user-name")
+    password = driver.find_element(By.ID, "password")
+    username.send_keys("standard_user")
+    password.send_keys("secret_sauce")
 
-login = driver.find_element(By.ID, "login-button")
-login.click()
+    login = driver.find_element(By.ID, "login-button")
+    login.click()
 
-print("Login Successful!")
-driver.quit()
+    print("Login Successful!")
+    driver.quit()
