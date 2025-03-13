311d70e1830ed24c5d9f3bddb121949432ac2e91
suting777 <suting.w777@gmail.com>
2025-03-13 19:16:40 +0000

    Added cross-browser testing script

---
+++ login_cross_brosers_selenium.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browers = ["Chrome","Firefox]
+
+for brower in browers
+    if brower == "Chorme":
+      driver = webdriver.Chrome()
+    else browser == "Firefox"
+      driver = webdriver.Firefox()
+
+driver.get("https://www.saucedemo.com/")
+print(running test on {browser}
+
+username = driver.find_element(By.ID, "user-name")
+password = driver.find_element(By.ID, "password")
+username.send_keys("standard_user")
+password.send_keys("secret_sauce")
+
+login = driver.find_element(By.ID, "login-button")
+login.click()
+
+print("Login Successful!")
+driver.quit()
